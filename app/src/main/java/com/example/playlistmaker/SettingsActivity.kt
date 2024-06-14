9645656818f897d52dabee2e6fa7b015a7036021
justannn <kiritseva@sfedu.ru>
2024-06-14 14:29:08 +0300

    Almost(only switch left) finished logic for Settings screen

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,16 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -18,6 +23,32 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<ImageButton>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
+        }
+
+
+        val shareButton = findViewById<Button>(R.id.shareButton)
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message))
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, null))
+        }
+
+        val supportButton = findViewById<Button>(R.id.supportButton)
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_mail)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_mail_subject))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_mail_message))
+            startActivity(intent)
+        }
+
+        val aggreementButton = findViewById<Button>(R.id.agreementButton)
+        aggreementButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(getString(R.string.agreement_uri))
+            startActivity(intent)
         }
     }
 }
