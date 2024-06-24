3c824df437f1df236a5ae3a7488a006567a19e31
justannn <kiritseva@sfedu.ru>
2024-06-24 16:21:50 +0300

    Some changes

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
@@ -13,9 +16,10 @@ import androidx.core.widget.addTextChangedListener
 import androidx.transition.Visibility
 
 class SearchActivity : AppCompatActivity() {
+
+    private var searchText: String = TEXT_DEF
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.backButton)
@@ -29,11 +33,29 @@ class SearchActivity : AppCompatActivity() {
             onTextChanged = { text, start, before, count ->
                 if (!text.isNullOrEmpty()) clearButton.visibility = View.VISIBLE
                 else clearButton.visibility = View.INVISIBLE
+                searchText = text.toString()
             }
         )
 
         clearButton.setOnClickListener {
             searchEditText.text.clear()
+            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(searchEditText.windowToken, 0)
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        findViewById<EditText>(R.id.searchEditText).setText(savedInstanceState.getString(SEARCH_TEXT))
+    }
+
+    companion object{
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val TEXT_DEF = ""
+    }
+
 }
