1b01fc6a94fb50783fd54dfdfe758348df2fb0e3
justannn <kiritseva@sfedu.ru>
2024-06-24 14:55:42 +0300

    Added logic of search screen

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,39 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import androidx.transition.Visibility
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageButton>(R.id.clearButton)
+        searchEditText.addTextChangedListener(
+            onTextChanged = { text, start, before, count ->
+                if (!text.isNullOrEmpty()) clearButton.visibility = View.VISIBLE
+                else clearButton.visibility = View.INVISIBLE
+            }
+        )
+
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
         }
     }
 }
