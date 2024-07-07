d4d679bf12b7bc31ad3a5f6f3c525acc88799558
justannn <kiritseva@sfedu.ru>
2024-07-07 16:51:42 +0300

    Added recycler view in SearchActivity and its logic: loading pictures, cutting long lines, showing placeholder image.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -13,6 +13,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.Visibility
 
 class SearchActivity : AppCompatActivity() {
@@ -41,6 +43,17 @@ class SearchActivity : AppCompatActivity() {
             searchEditText.text.clear()
             (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(searchEditText.windowToken, 0)
         }
+
+        val recyclerView : RecyclerView = findViewById(R.id.searchRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = TrackAdapter(
+            tracks = listOf(
+                Track("Smells Like Teen Spirit", "Nirvana", "5:01", ""),
+                Track("Billie Jean(the best song in this world forever in my heart)", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+                Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+                Track("Whole Lotta Love", "Led Zeppelin(best group in the world)", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+                Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
+        )
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -52,6 +65,7 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         findViewById<EditText>(R.id.searchEditText).setText(savedInstanceState.getString(SEARCH_TEXT))
     }
+
 
     companion object{
         const val SEARCH_TEXT = "SEARCH_TEXT"
