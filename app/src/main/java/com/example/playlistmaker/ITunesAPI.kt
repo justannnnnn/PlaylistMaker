6cad3ffc12678bb6448a260fb7029ded13725477
justannn <kiritseva@sfedu.ru>
2024-07-25 13:26:28 +0300

    Added search logic(ITunes API):  - search of tracks  - exceptions(no internet, no tracks)  - clearing of search query and its editing

---
+++ app/src/main/java/com/example/playlistmaker/ITunesAPI.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmaker
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ITunesAPI {
+
+    @GET("/search?entity=song")
+    fun searchSongs(@Query("term") query: String): Call<TrackSearchResponse>
+
+}
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,10 +4,17 @@ import android.content.Context
 import android.inputmethodservice.InputMethodService
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,10 +23,35 @@ import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.Visibility
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
     private var searchText: String = TEXT_DEF
+
+    private val baseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val tracksService = retrofit.create(ITunesAPI::class.java)
+
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackAdapter()
+
+    private lateinit var placeholderLL: LinearLayout
+    private lateinit var placeholderTextView: TextView
+    private lateinit var placeholderImageView: ImageView
+    private lateinit var refreshButton: Button
+    private lateinit var recyclerView: RecyclerView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -35,25 +67,41 @@ class SearchActivity : AppCompatActivity() {
             onTextChanged = { text, start, before, count ->
                 if (!text.isNullOrEmpty()) clearButton.visibility = View.VISIBLE
                 else clearButton.visibility = View.INVISIBLE
+                tracks.clear()
+                adapter.notifyDataSetChanged()
+                placeholderLL.visibility = View.GONE
                 searchText = text.toString()
             }
         )
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE){
+                if (searchText.isNotEmpty()) search()
+                true
+            }
+            false
+        }
 
         clearButton.setOnClickListener {
             searchEditText.text.clear()
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderLL.visibility = View.GONE
             (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(searchEditText.windowToken, 0)
         }
 
-        val recyclerView : RecyclerView = findViewById(R.id.searchRecyclerView)
+        placeholderLL = findViewById(R.id.placeholderLL)
+        placeholderLL.visibility = View.GONE
+        placeholderImageView = findViewById(R.id.placeholderIV)
+        placeholderTextView = findViewById(R.id.placeholderTextView)
+        refreshButton = findViewById(R.id.refreshButton)
+        refreshButton.setOnClickListener {
+            search()
+        }
+
+        recyclerView = findViewById(R.id.searchRecyclerView)
+        adapter.tracks = tracks
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TrackAdapter(
-            tracks = listOf(
-                Track("Smells Like Teen Spirit", "Nirvana", "5:01", ""),
-                Track("Billie Jean(the best song in this world forever in my heart)", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-                Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-                Track("Whole Lotta Love", "Led Zeppelin(best group in the world)", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-                Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"))
-        )
+        recyclerView.adapter = adapter
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -64,6 +112,47 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         findViewById<EditText>(R.id.searchEditText).setText(savedInstanceState.getString(SEARCH_TEXT))
+    }
+
+    private fun search(){
+        tracksService.searchSongs(searchText)
+            .enqueue(object : Callback<TrackSearchResponse> {
+                override fun onResponse(
+                    call: Call<TrackSearchResponse>,
+                    response: Response<TrackSearchResponse>
+                ) {
+                    if (response.code() == 200){
+                        if (response.body()?.results?.isNotEmpty() == true){
+                            showPlaceholder(null, null)
+                            tracks.clear()
+                            tracks.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                        else {
+                            showPlaceholder(R.string.nothing_found, R.drawable.nothing_found)
+                        }
+                    }
+                    else showPlaceholder(R.string.net_problems, R.drawable.no_internet)
+                }
+
+                override fun onFailure(call: Call<TrackSearchResponse>, t: Throwable) {
+                    showPlaceholder(R.string.net_problems, R.drawable.no_internet)
+                }
+
+            })
+    }
+
+    private fun showPlaceholder(@StringRes message: Int?, @DrawableRes icon: Int?){
+        if (message != null && icon != null){
+            if (message == R.string.net_problems) refreshButton.visibility = View.VISIBLE
+            else refreshButton.visibility = View.GONE
+            placeholderLL.visibility = View.VISIBLE
+            placeholderImageView.setImageResource(icon)
+            placeholderTextView.setText(message)
+        }
+        else{
+            placeholderLL.visibility = View.GONE
+        }
     }
 
 
